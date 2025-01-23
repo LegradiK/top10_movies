028189d1bafc155ff970da5d84a42f504a094769
Kaho Legradi <legradi.kaho@gmail.com>
2025-01-23 21:58:35 +0000

    created movie database

--- main.py
+++ main.py
@@ -8,27 +8,56 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///top10-movies.db'
 Bootstrap5(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
 
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False) 
+    description: Mapped[str] = mapped_column(String(250), nullable=False) 
+    rating: Mapped[float] = mapped_column(Float, nullable=False) 
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+
+# second_movie = Movie(
+#     title="Avatar The Way of Water",
+#     year=2022,
+#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     rating=7.3,
+#     ranking=9,
+#     review="I liked the water.",
+#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+# )
+
+
+# with app.app_context():
+#     db.session.add(second_movie)
+#     db.session.commit()
 
 
 @app.route("/")
